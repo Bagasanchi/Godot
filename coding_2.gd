3892497abea330b0628b6eef2485e008d3eac14f
Bagasanchi <bagaezwin@gmail.com>
2025-10-29 17:19:57 +0800

    No changes

--- coding_2.gd
+++ coding_2.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+func _ready():
+	position.x = 1000
+
 func _process(delta: float):
-	#$Sprite2D.rotate(0.5)
-	$Sprite2D.position.x = $Sprite2D.position.x + 1
+	$Sprite2D.rotate(0.1)
+	#$Sprite2D.position.x += 10
+	#exercise
+	$ExerciseSprite2D.position.x += 5
+	$ExerciseSprite2D.position.y += 3
+	$ExerciseSprite2D.scale.y *= 1.005
+	$ExerciseSprite2D.scale.x *= 1.005

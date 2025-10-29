125ac1cce5b390dc1155dc79fe24dac3099e4d99
Bagasanchi <bagaezwin@gmail.com>
2025-10-29 18:02:21 +0800

    Bounce modifications

--- coding_3_boolean.gd
+++ coding_3_boolean.gd
@@ -1,4 +1,17 @@
 extends Node2D
 
+var a := 0
+var direction := 1
+var speed := 15
+
 func _ready():
-	print( 5 > 4 )
+	while a < 10:
+		print("while loop is active")
+		a += 1
+
+func _process(delta: float) -> void:
+	$Icon.position.x += speed * direction
+	if $Icon.position.x >= 1152:
+		direction = -1
+	if $Icon.position.x <= 0:
+		direction = 1

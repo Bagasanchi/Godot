a1e1f79f03d7608003f3fdf2e73f1271f1677384
Bagasanchi <bagaezwin@gmail.com>
2025-10-30 08:12:34 +0800

    up and down

--- coding_3_boolean.gd
+++ coding_3_boolean.gd
@@ -10,8 +10,13 @@ func _ready():
 		a += 1
 
 func _process(delta: float) -> void:
-	$Icon.position.x += speed * direction
+	$Icon.position.x += (2+speed) * direction
+	$Icon.position.y += (speed/2) * direction
 	if $Icon.position.x >= 1152:
 		direction = -1
 	if $Icon.position.x <= 0:
+		direction = 1
+	if $Icon.position.y >= 648:
+		direction = -1
+	if $Icon.position.y <= 0:
 		direction = 1
